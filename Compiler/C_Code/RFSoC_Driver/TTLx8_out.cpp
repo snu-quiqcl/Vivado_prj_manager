acd95119bab8f3a40e42920a53c4ba6c08e062da
ParkJeonghyun <alexist@snu.ac.kr>
2023-10-06 17:52:55 +0900

    ttlx8 is impossible to measure since it is to fast. High resolution with impedance matched oscilloscope is needed

--- Compiler/C_Code/RFSoC_Driver/TTLx8_out.cpp
+++ Compiler/C_Code/RFSoC_Driver/TTLx8_out.cpp
@@ -1,20 +1,30 @@
 #include "RFSoC_Driver.h"
 
 void TTLx8_out::set(uint64_t pulse){
-    if( ( ( this -> last_output_time ) >> 3) < get_timestamp() ){
-        uint64_t temp_last_pulse = this->last_pulse;
+    xil_printf("timestamp %d timestamp corase %d last pulse %d\r\n",get_timestamp(), get_timestamp_coarse(), this->last_output_time);
+
+    if( ( ( this -> last_output_time ) >> 3) < get_timestamp_coarse() ){
+        uint64_t temp_last_pulse = (this->last_pulse >> 7 )& 0x1;
         this->last_pulse = 0;
 
         for( int i = 0; i < (get_timestamp() % 8); i ++ ){
             this-> last_pulse += (temp_last_pulse << i);
+            xil_printf("last pulse update: %llx\r\n",this->last_pulse);
         }
 
-        this->last_pulse += ( pulse << ( get_timestamp() % 8 ) ) ;
+        this->last_pulse += this->last_pulse | ( pulse << ( get_timestamp() % 8 ) ) ;
         this->last_output_time = get_timestamp();
     }
     else{
-        this->last_pulse += ( pulse << ( get_timestamp() % 8 ) );
+        int i = (get_timestamp() % 8);
+        for( i ; i < 8 ; i++){
+            this->last_pulse = (this->last_pulse & ( 0xff - (1 << i) )) | ( pulse << i );
+        }
+        this->last_output_time = get_timestamp();
     }
+
+    xil_printf("timestamp %d timestamp corase %d last pulse %d\r\n",get_timestamp(), get_timestamp_coarse(), this->last_output_time);
+    xil_printf("%llx\r\n",this->last_pulse);
 
     Xil_Out128(this->addr,MAKE128CONST( get_timestamp_coarse(), this->last_pulse));
     return;
--- Compiler/C_Code/skeleton_code/skeleton_code.cpp
+++ Compiler/C_Code/skeleton_code/skeleton_code.cpp
@@ -5,23 +5,56 @@ int main(){
     dac_0.set_addr(XPAR_DAC_CONTROLLER_0_BASEADDR);
     dac_0.flush_fifo();
 
+    DAC dac_1;
+    dac_1.set_addr(XPAR_DAC_CONTROLLER_1_BASEADDR);
+    dac_1.flush_fifo();
+
+    DAC dac_2;
+    dac_2.set_addr(XPAR_DAC_CONTROLLER_2_BASEADDR);
+    dac_2.flush_fifo();
+
+    DAC dac_3;
+    dac_3.set_addr(XPAR_DAC_CONTROLLER_3_BASEADDR);
+    dac_3.flush_fifo();
+
+    DAC dac_4;
+    dac_4.set_addr(XPAR_DAC_CONTROLLER_4_BASEADDR);
+    dac_4.flush_fifo();
+
+    DAC dac_5;
+    dac_5.set_addr(XPAR_DAC_CONTROLLER_5_BASEADDR);
+    dac_5.flush_fifo();
+
+    DAC dac_6;
+    dac_6.set_addr(XPAR_DAC_CONTROLLER_6_BASEADDR);
+    dac_6.flush_fifo();
+
+    DAC dac_7;
+    dac_7.set_addr(XPAR_DAC_CONTROLLER_7_BASEADDR);
+    dac_7.flush_fifo();
+
     TTL_out ttl_out_0(XPAR_TTL_OUT_0_BASEADDR);
     ttl_out_0.flush_fifo();
 
     TTL_out ttl_out_1(XPAR_TTL_OUT_1_BASEADDR);
     ttl_out_1.flush_fifo();
 
+    TTL_out ttl_out_2(XPAR_TTL_OUT_2_BASEADDR);
+    ttl_out_2.flush_fifo();
+
     TTLx8_out ttlx8_out_0(XPAR_TTLX8_OUT_0_BASEADDR);
     ttlx8_out_0.flush_fifo();
-
-    TTLx8_out ttlx8_out_1(XPAR_TTLX8_OUT_1_BASEADDR);
-    ttlx8_out_1.flush_fifo();
 
     TimeController tc_0(XPAR_TIMECONTROLLER_0_BASEADDR);
     tc_0.auto_stop();
     tc_0.reset();
-    
-    dac_0.set_config(1.0,10000,0,0);
 
+    ttlx8_out_0.set(1);
+    delay(30);
+    ttlx8_out_0.set(0);
+    delay(40);
+    ttlx8_out_0.set(1);
+    delay(50);
+    ttlx8_out_0.set(0);
     tc_0.auto_start();
 }

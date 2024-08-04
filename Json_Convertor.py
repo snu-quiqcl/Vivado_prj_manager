d50f995cf0877a92a690b77f157d93ba53349535
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-08-05 07:48:08 +0900

    Ignore

--- Json_Convertor.py
+++ Json_Convertor.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     input_str = """
 CONFIG.C_BUFGCE_DIV                 string   false      1
 CONFIG.C_BUFG_GT_SYNC               string   false      false
-CONFIG.C_BUF_TYPE                   string   false      OBUFDS
+CONFIG.C_BUF_TYPE                   string   false      IBUFDS
 CONFIG.C_OBUFDS_GTE5_ADV            string   false      "00"
 CONFIG.C_REFCLK_ICNTL_TX            string   false      "00000"
 CONFIG.C_SIZE                       string   false      1
